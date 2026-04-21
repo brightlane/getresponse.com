9fc56d8e854ea003994a701313cf54531fd281df
brightlane <benitopalmarini@aol.com>
2026-04-21 17:55:37 -0400

    Create db-utils.js

---
+++ ib/db-utils.js
@@ -0,0 +1,20 @@
+// lib/db-utils.js
+const getKeywords = () => [];
+const appendKeywords = (keywords, strategy) => {};
+const saveArticle = (article, strategy) => {};
+const logObservation = (observation) => {};
+const getLatestStrategy = (strategy) => strategy;
+const getRankedPages = () => [];
+const getArticlesForRepair = () => [];
+const markArticleAsRepaired = (article, strategy) => {};
+
+module.exports = {
+  getKeywords,
+  appendKeywords,
+  saveArticle,
+  logObservation,
+  getLatestStrategy,
+  getRankedPages,
+  getArticlesForRepair,
+  markArticleAsRepaired
+};
